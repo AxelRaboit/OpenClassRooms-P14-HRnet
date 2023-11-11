ac9ac2efc2c651f1574d90aa9a1d078f074ae01c
AxelRaboit <axel.raboit@gmail.com>
2023-11-11 14:49:04 +0100

    [FIX] Indent in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {Home, Employees} from "./pages";
+import { Home, Employees } from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
@@ -12,7 +12,7 @@ import "./reset.css";
 let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
+  <Provider store={store}>
     <PersistGate persistor={persistor}>
       <BrowserRouter>
         <Routes>
@@ -20,6 +20,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <Route path="/employees" element={<Employees />} />
         </Routes>
       </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    </PersistGate>
+  </Provider>
 );
